936a1213261d1916ffdba5f280ad61c6afc14b0a
Andrew Hong <35881688+NovialRiptide@users.noreply.github.com>
2022-01-07 14:33:11 -0500

    Fixed platform spacing

--- Assets/Scripts/WorldGen.cs
+++ Assets/Scripts/WorldGen.cs
@@ -6,24 +6,19 @@ using UnityEngine.Tilemaps;
 public class WorldGen : MonoBehaviour
 {
     public float spawnPlatformDistance = 200f;
-    public float maxJumpDistance = 3f;
+    public float minJumpDistance = 2f;
+    public float maxJumpDistance = 5f;
     public GameObject player;
     public GameObject[] platforms;
 
     private Vector3 previousPlatformPosition;
 
-    private void Awake()
-    {
-        createPlatform(0, new Vector3(10, 0, 0));
-        createPlatform(0, new Vector3(3, 0, 0));
-    }
-
     private void Update()
     {
         if (Vector3.Distance(player.transform.position, previousPlatformPosition) < spawnPlatformDistance)
         {
-            Vector3 newPosition = new Vector3(Random.Range(0, maxJumpDistance), 0, 0);
-            createPlatform(Random.Range(0, platforms.Length), newPosition);
+            Vector3 newPosition = new Vector3((int) Random.Range(minJumpDistance, maxJumpDistance), 0, 0);
+            createPlatform((int) Random.Range(0, platforms.Length), newPosition);
         }
     }
 
@@ -34,5 +29,6 @@ public class WorldGen : MonoBehaviour
         GameObject spawnedPlatform = Instantiate(platform, previousPlatformPosition + position, Quaternion.identity);
         previousPlatformPosition = spawnedPlatform.transform.position + size;
         previousPlatformPosition.y -= spawnedPlatform.GetComponentInChildren<Tilemap>().size.y;
+        spawnedPlatform.transform.SetParent(transform);
     }
 }
